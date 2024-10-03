85d78b61bd8e8da0e043a515901636c9ed326b81
oblivion4us <oblivion4us@icloud.com>
2024-10-03 14:26:29 +0200

    add code for dz.csv part

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ df = pd.read_csv("top_rated_9000_movies_on_TMDB.csv")
 print(df.head())
 print(df.info())
 print(df.describe())
+
+df_dz = pd.read_csv("dz.csv")
+print(f"{df_dz}\n")
+
+df_dz.fillna(value=0, inplace=True)
+print(f"{df_dz}\n")
+
+group = df_dz.groupby('City')['Salary'].mean()
+print(group)
